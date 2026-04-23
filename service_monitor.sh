f1daa9e84afae1e8513379c6b6fc42803503ed33
Jaybhuke <Jaybhuke97@gmail.com>
2026-04-23 19:29:42 +0000

    Added telegram alert in script

--- service_monitor.sh
+++ service_monitor.sh
@@ -4,6 +4,10 @@ SERVICE="nginx"
 LOG_FILE="/home/ec2-user/learning/projects/smp3/monitors.log"
 
 
+set -a
+source /home/ec2-user/learning/projects/smp3/.env
+set +a
+
 DATE=$(date)
 HOSTNAME=$(hostname)
 
@@ -14,11 +18,20 @@ STATUS=$(systemctl is-active $SERVICE)
 
 
 if [ $STATUS != "active" ]; then
+	
+	MESSAGE="$SERVICE is down on $HOSTNAME at $DATE. Restarting"
 
-	echo "$SERVICE is down at $DATE" >> $LOG_FILE
+	echo "$MESSAGE" >> $LOG_FILE
 
 	sudo systemctl start $SERVICE
 
-	echo "Attempted to restart $SERVICE" >> $LOG_FILE
+	curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+		--data-urlencode "chat_id=$CHAT_ID" \
+		--data-urlencode "text=$MESSAGE"
+
+	echo "$SERVICE restarted" >> $LOG_FILE
+else
+
+	echo "$SERVICE is running" >> $LOG_FILE
 		
 fi
